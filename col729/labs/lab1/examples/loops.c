742b20f0625305454bda94a3b0dd75d77ef43070
Ankush Phulia <ankush.phulia22@gmail.com>
2019-01-10 17:33:46 +0530

    editted the examples in col729/lab1

---
+++ col729/labs/lab1/examples/loops.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_sorted(int *a, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void add_arrays(int *a, int *b, int *c, int n) {
+    for (int i = 0; i < n; i++) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+int main() {
+    return 0;
+}
